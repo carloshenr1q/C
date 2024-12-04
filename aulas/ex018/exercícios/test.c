0576a4bdb5d55c61b43d9ac6a467386dd04ec76b
henr1q <carloshav44@gmail.com>
2024-12-04 14:09:29 -0300

    alterações

--- aulas/ex018/exercícios/test.c
+++ aulas/ex018/exercícios/test.c
@@ -32,19 +32,38 @@ int main() {
         scanf("%49[^\n]s", lista[y].password);
         fflush(stdin);
     }
-    
+    system("cls");
     printf("\n");
     printf("============ ACESSO NACIONAL PF ============\n\n");
 
     puts("Digite nome de usuário: ");
     scanf("%49[^\n]s", user1);
     fflush(stdin);
+    printf("\n");
+    z = strcmp(lista[1].user, user1);
 
-    z = strcmp(lista[0].user, user1);
     if (z == 0) {
-        printf("Usuário encontrado!\n");
+        printf("Usuário encontrado!\n\n");
+
+        puts("Digite a senha: ");
+        scanf("%49[^\n]s", password1);
+        fflush(stdin);
+        printf("\n");
+        z = strcmp(lista[1].password, password1);
+
+        if (z == 0)
+        {
+            system("cls");
+            printf("============ BEM-VINDO AO ACESSO DA PF, %s ============", user1);
+        }
+        else{
+            printf("Senha Incorreta.");
+            return 1;
+        }
+        
     } else {
         printf("Usuário não encontrado.\n");
+        return 1;
     }
 
     return 0;
